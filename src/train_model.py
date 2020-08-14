4aa1ecbc6f87ef4dc5569f551023a47eaafdc969
sabarishchockalingam2017 <sabarishchockalingam2017@u.northwestern.edu>
2020-08-14 10:17:54 -0500

    working with fasttext

--- src/train_model.py
+++ src/train_model.py
@@ -6,9 +6,6 @@ from sklearn.linear_model import LogisticRegression
 # from sklearn.model_selection import cross_val_score
 import pandas as pd
 import fasttext
-# from gensim.models import Word2Vec
-# import string
-# import nltk
 
 'This script loads and processes data, and then trains and saves the model'
 
@@ -24,13 +21,6 @@ corpus_path = path.join(data_path, 'yelp_academic_dataset_review.json')
 corpus_df = jsontodf(corpus_path, n_lines=50000)
 corpus_df = corpus_df[['text', 'stars']]
 
-# normalizing text
-#corpus_df['normalized'] = normalize_corpus(corpus_df['text'])
-
-# # w2vmodel = Word2Vec(reviewdf['normalized'],
-# #                     size=50,
-# #                     min_count=10)
-#
 tfidf = TfidfVectorizer(sublinear_tf=True, min_df=500, max_df=7000,
                         norm='l2', encoding='utf8',
                         ngram_range=(1, 2),
@@ -44,11 +34,8 @@ traindf = pd.get_dummies(corpus_df, columns=['stars'])
 #                            max_iter=1000)
 # logreg.fit(features, corpus_df['stars'])
 
-fast
+ftmodel = fasttext
 
-# CV_n = 5
-# accuracies = cross_val_score(model, features, labels, scoring='accuracy', cv=CV_n)
-#
 
 
 

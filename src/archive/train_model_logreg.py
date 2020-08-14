398a180f0aad8d6b23ef5b49b30e9f743c01bc89
sabarishchockalingam2017 <sabarishchockalingam2017@u.northwestern.edu>
2020-08-14 10:27:50 -0500

    archived log reg

---
+++ src/archive/train_model_logreg.py
@@ -0,0 +1,47 @@
+from config import PROJECT_HOME
+from os import path
+from src.helpers.data_processing import jsontodf, normalize_corpus
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
+# from sklearn.model_selection import cross_val_score
+import pandas as pd
+import fasttext
+
+'This script loads and processes data, and then trains and saves the model'
+
+
+# data_folder = 'twitter_dataset'
+# data_file = 'twitter_sentiment.csv'
+
+# file paths
+data_path = path.join(PROJECT_HOME, 'data', 'yelp_dataset')
+corpus_path = path.join(data_path, 'yelp_academic_dataset_review.json')
+
+# loading data
+corpus_df = jsontodf(corpus_path, n_lines=50000)
+corpus_df = corpus_df[['text', 'stars']]
+
+tfidf = TfidfVectorizer(sublinear_tf=True, min_df=500, max_df=7000,
+                        norm='l2', encoding='utf8',
+                        ngram_range=(1, 2),
+                        stop_words='english')
+
+features = tfidf.fit_transform(corpus_df['text']).toarray()
+traindf = pd.get_dummies(corpus_df, columns=['stars'])
+
+# logreg = LogisticRegression(solver='lbfgs',
+#                            multi_class='multinomial',
+#                            max_iter=1000)
+# logreg.fit(features, corpus_df['stars'])
+
+ftmodel = fasttext
+
+# CV_n = 5
+# accuracies = cross_val_score(model, features, labels, scoring='accuracy', cv=CV_n)
+#
+
+
+
+
+
+

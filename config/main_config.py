3b696b8a4c165e9181d1d386853291a578815106
sabarishchockalingam2017 <sabarishchockalingam2017@u.northwestern.edu>
2020-08-24 15:37:24 -0500

    updated log directory to have posix path and add rotating file handler for logging

--- config/main_config.py
+++ config/main_config.py
@@ -1,4 +1,7 @@
 from os import path
 from os.path import dirname
-
+from pathlib import Path
+import pathlib
 PROJECT_HOME = dirname(dirname(path.abspath(__file__)))
+# needed path module as fileconfig file use unix parsing for writing log file
+LOGGING_PATH = pathlib.PurePosixPath(Path(path.join(PROJECT_HOME,"logs","run_log.log")))

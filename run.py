4dd174b006bfb85f092fc262fecc86385806c43a
sabarishchockalingam2017 <sabarishchockalingam2017@u.northwestern.edu>
2020-08-25 12:52:55 -0500

    moved comment run.py

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
-''' This is a central location to run all files'''
 from app import app
 from config.main_config import LOGGING_PATH, LOGGING_CONFIG
 import logging.config
 
+''' This is a central location to run all files'''
 logging.config.fileConfig(LOGGING_CONFIG,
                           disable_existing_loggers=False,
                           defaults={'log_dir': LOGGING_PATH})

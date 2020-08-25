9695529f98d5c36d6d3657d4aae4864993074bf2
sabarishchockalingam2017 <sabarishchockalingam2017@u.northwestern.edu>
2020-08-25 12:44:57 -0500

    seperated configuration information to main and flask in order to use app.config flask function

--- config/main_config.py
+++ config/main_config.py
@@ -2,6 +2,10 @@ from os import path
 from os.path import dirname
 from pathlib import Path
 import pathlib
+
+'This file contains configuration details for general use in all files'
+
 PROJECT_HOME = dirname(dirname(path.abspath(__file__)))
 # needed path module as fileconfig file use unix parsing for writing log file
-LOGGING_PATH = pathlib.PurePosixPath(Path(path.join(PROJECT_HOME,"logs","run_log.log")))
+LOGGING_PATH = pathlib.PurePosixPath(Path(path.join(PROJECT_HOME, "logs", "run_log.log")))
+LOGGING_CONFIG = Path(PROJECT_HOME, "config", "logging", "local.conf")

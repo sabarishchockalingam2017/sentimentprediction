2a9c65d004550c97f73a0f24e67f1a91548627b0
sabarishchockalingam2017 <sabarishchockalingam2017@u.northwestern.edu>
2020-10-22 12:10:16 -0500

    added model path

--- config/main_config.py
+++ config/main_config.py
@@ -9,3 +9,4 @@ PROJECT_HOME = dirname(dirname(path.abspath(__file__)))
 # needed path module as fileconfig file use unix parsing for writing log file
 LOGGING_PATH = pathlib.PurePosixPath(Path(path.join(PROJECT_HOME, "logs", "run_log.log")))
 LOGGING_CONFIG = Path(PROJECT_HOME, "config", "logging", "local.conf")
+MODEL_PATH = path.join(PROJECT_HOME,'models', 'ft_model.bin')

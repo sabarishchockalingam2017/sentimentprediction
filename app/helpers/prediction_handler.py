16dee602c603e6d5f29f6605163f7767102d1807
sabarishchockalingam2017 <sabarishchockalingam2017@u.northwestern.edu>
2020-11-03 13:29:09 -0600

    working on potential s3 glacier load

--- app/helpers/prediction_handler.py
+++ app/helpers/prediction_handler.py
@@ -2,6 +2,8 @@ from config.main_config import MODEL_PATH
 import fasttext
 import string
 import logging.config
+import boto3
+
 
 ''' This file contains functions to take in input and return predictions.'''
 
@@ -17,3 +19,25 @@ def get_prediction(user_input):
     user_input = user_input.translate(str.maketrans('', '', string.punctuation))
     model_out=spmodel.predict(user_input)
     return model_out[0][0][9]
+
+def glacier_load():
+    'loads model from AWS S3 glacier'
+    cred = boto3.Session(profile_name='default2').get_credentials()
+    ACCESS_KEY = cred.access_key
+    SECRET_KEY = cred.secret_key
+    print(ACCESS_KEY)
+
+    glacier = boto3.client('glacier',
+                            aws_access_key_id=ACCESS_KEY,
+                            aws_secret_access_key=SECRET_KEY
+                            )
+
+    job_params = {'Type': 'inventory-retrieval',
+                  'ArchiveId':'irx1pLNvoBxd0qlLN-eTGVF6zeLZ4ARXeZFo-gLbVCTmqXmz7COGqvO6qwdVLMUmT1bSogVEGQDca_Nd18g9o3LnusOfiw-n1m9k0GtrY6LHjJzSqESpDr0BMP8laxtgGd6WSNw_zg'}
+    response = glacier.initiate_job(vaultName='arn:aws:glacier:us-east-2:351282577611:vaults/sentpred',
+                                    jobParameters=job_params)
+    #response = glacier.describe_vault(vaultName='arn:aws:glacier:us-east-2:351282577611:vaults/sentpred')
+    body = response['Body'].read()
+
+if __name__=='__main__':
+    glacier_load()

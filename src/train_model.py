7bdd0f5fffeb73eefcbc3519f32b4dd5a65f9a7d
sabarishchockalingam2017 <sabarishchockalingam2017@u.northwestern.edu>
2020-11-04 11:34:34 -0600

    edited to train on 50000 yelp data set rows and save model as ft_model

--- src/train_model.py
+++ src/train_model.py
@@ -1,21 +1,12 @@
-from config.main_config import PROJECT_HOME, LOGGING_PATH, LOGGING_CONFIG
+from config.main_config import PROJECT_HOME
 from os import path
-from pathlib import Path
-from src.helpers.data_processing import jsontodf, ft_preproc, ft_predict, normalize_corpus
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.linear_model import LogisticRegression
-# from sklearn.model_selection import cross_val_score
-import pandas as pd
+from src.helpers.data_processing import jsontodf, ft_preproc, ft_predict
 import fasttext
-import logging
 import logging.config
 
 'This script loads and processes data, and then trains and saves the model'
 
 logger = logging.getLogger('train_model')
-
-# data_folder = 'twitter_dataset'
-# data_file = 'twitter_sentiment.csv'
 
 # file paths
 data_path = path.join(PROJECT_HOME, 'data', 'yelp_dataset')
@@ -25,10 +16,10 @@ corpus_path = path.join(data_path, 'yelp_academic_dataset_review.json')
 corpus_df = jsontodf(corpus_path, n_lines=50000)
 corpus_df = corpus_df[['text', 'stars']]
 
-# # preprocessing to format for fasttext
-# ft_inputfile = ft_preproc(corpus_df['stars'], corpus_df['text'])
-# ft_model = fasttext.train_supervised(input=ft_inputfile, lr=1, dim=300, wordNgrams=3)
-# logger.warning("Model trained.")
-# model_save_path = path.join(PROJECT_HOME, 'models', 'ft_model.bin')
-# # ft_model.save_model(model_save_path)
-# # ft_results = ft_predict(ft_model, ['Good restaurant.', 'Bad restaurant.'])
+# preprocessing to format for fasttext
+ft_inputfile = ft_preproc(corpus_df['stars'], corpus_df['text'])
+ft_model = fasttext.train_supervised(input=ft_inputfile, lr=1, dim=300, wordNgrams=3)
+logger.warning("Model trained.")
+model_save_path = path.join(PROJECT_HOME, 'models', 'ft_model.bin')
+ft_model.save_model(model_save_path)
+ft_results = ft_predict(ft_model, ['Good restaurant.', 'Bad restaurant.'])

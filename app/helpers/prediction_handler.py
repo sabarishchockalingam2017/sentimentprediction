922bbba1eeefd56037c96d72149427c27f0f9fab
sabarishchockalingam2017 <sabarishchockalingam2017@u.northwestern.edu>
2020-11-04 11:35:18 -0600

    working on s3 loading, for now is still local

--- app/helpers/prediction_handler.py
+++ app/helpers/prediction_handler.py
@@ -3,13 +3,12 @@ import fasttext
 import string
 import logging.config
 import boto3
-
-
 ''' This file contains functions to take in input and return predictions.'''
 
 logger = logging.getLogger('prediction_handler')
 
-#loading sentiment prediction model
+
+# loading sentiment prediction model
 spmodel = fasttext.load_model(MODEL_PATH)
 logger.info("Sentiment prediction model loaded.")
 
@@ -20,24 +19,20 @@ def get_prediction(user_input):
     model_out=spmodel.predict(user_input)
     return model_out[0][0][9]
 
-def glacier_load():
-    'loads model from AWS S3 glacier'
+def load_model():
+    'Returns loaded model object from s3 object'
     cred = boto3.Session(profile_name='default2').get_credentials()
     ACCESS_KEY = cred.access_key
     SECRET_KEY = cred.secret_key
-    print(ACCESS_KEY)
 
-    glacier = boto3.client('glacier',
+    s3client = boto3.client('s3',
                             aws_access_key_id=ACCESS_KEY,
                             aws_secret_access_key=SECRET_KEY
                             )
 
-    job_params = {'Type': 'inventory-retrieval',
-                  'ArchiveId':'irx1pLNvoBxd0qlLN-eTGVF6zeLZ4ARXeZFo-gLbVCTmqXmz7COGqvO6qwdVLMUmT1bSogVEGQDca_Nd18g9o3LnusOfiw-n1m9k0GtrY6LHjJzSqESpDr0BMP8laxtgGd6WSNw_zg'}
-    response = glacier.initiate_job(vaultName='arn:aws:glacier:us-east-2:351282577611:vaults/sentpred',
-                                    jobParameters=job_params)
-    #response = glacier.describe_vault(vaultName='arn:aws:glacier:us-east-2:351282577611:vaults/sentpred')
-    body = response['Body'].read()
+    response = s3client.get_object(Bucket='sentpred', Key='ft_model.bin')
 
-if __name__=='__main__':
-    glacier_load()
+    body = response['Body'].read()
+    loaded_model = fasttext.load_model(body)
+    return loaded_model
+
